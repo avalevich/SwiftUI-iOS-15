6c56bb5583b8396f9aaaa79655302501116ab710
avalevich <alyaxey.valevich@allegro.com>
2024-07-22 21:14:50 +0200

    set up images and icons

---
+++ SwiftUI-iOS-15/SwiftUI_iOS_15App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_iOS_15App.swift
+//  SwiftUI-iOS-15
+//
+//  Created by Alex on 22/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_iOS_15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
